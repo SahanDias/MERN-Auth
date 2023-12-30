12cac09ea0ded843349ec8bdaf6609d763dcde54
SahanDias <sahankaushalya1102@gmail.com>
2023-12-30 12:20:21 +0530

    Create test API route

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
+import userRoutes from './routes/user.route.js';
+
 
 mongoose.connect("mongodb://localhost:27017/Users").then(() =>{
     console.log("Connected to the MongoDB");
@@ -12,3 +14,6 @@ const app= express();
 app.listen(3000, () =>{
     console.log('Server listening on port 3000');
 });
+
+app.use("/api/user", userRoutes);
+
