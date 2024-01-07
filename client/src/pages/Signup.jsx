220516e35d03c9a72b277818f4d553d2893e5696
SahanDias <sahankaushalya1102@gmail.com>
2024-01-07 18:51:20 +0530

    Complete signin page funactionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,12 +1,13 @@
 import { set } from 'mongoose';
 import { useState } from 'react';
-import { Link} from 'react-router-dom';
+import { Link,useNavigate} from 'react-router-dom';
 
 
 export default function Signup() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({...formData, [e.target.id]: e.target.value});
 
@@ -31,7 +32,9 @@ export default function Signup() {
       setError(true);
       return;
     }
-    
+
+
+    navigate('/sign-in');
     } catch (error) {
       setLoading(false);
       setError(true);
