d05cf986a9d5fbacce2bdec25a9f878f1d59af08
SahanDias <sahankaushalya1102@gmail.com>
2024-01-26 01:12:00 +0530

    Complete Home Page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text-center text-3xl font-bold
+      mb-4 text-slate-700'>
+        Welcome to Teasy Pay App</h1>
     </div>
   )
 }
